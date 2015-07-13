76064cbeee9c1385db19bf1b5357181f12c66f82
Jesse J. Anderson <jessejanderson@gmail.com>
2015-07-12 18:04:37 -0700

    fix winPercentage decimal placement

--- game.rb
+++ game.rb
@@ -1,6 +1,7 @@
 class Game
   attr_accessor :fights, :fights_won
   attr_reader :name
+
   def initialize(name)
     @name       = name
     @fights     = 0
@@ -12,6 +13,6 @@ class Game
   end
 
   def winPercentage
-    fights_won.to_f / fights.to_f
+    fights_won.to_f / fights.to_f * 100
   end
 end
